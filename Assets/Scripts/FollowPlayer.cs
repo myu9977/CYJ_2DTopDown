fba0faae78fc06589a02c5d34eb9b726ed5d255b
myu9977 <148519904+myu9977@users.noreply.github.com>
2024-05-10 17:02:51 +0900

    카메라가 플레이어 따라다님
    카메라에 부착하여 따라다니도록 할 스크립트를 작성했으나, 카메라를 플레이어의 자식으로 넣기만 해도 단순하게 따라다닐 수가 있어서 스크립트 내용은 비활성화 함.

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    /*
+    public Transform player; // 플레이어의 위치
+    public Vector2 offset; // 카메라의 위치 오프셋. 지금은 기본값인 0, 0
+
+    void Update()
+    {
+        // 카메라의 위치를 플레이어의 위치와 offset만큼 떨어진 곳으로 설정
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, transform.position.z);
+    }
+    */
+
+}

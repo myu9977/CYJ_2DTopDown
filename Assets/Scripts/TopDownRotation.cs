61a4e6c15f942d762060a0920b757556cffa0564
myu9977 <148519904+myu9977@users.noreply.github.com>
2024-05-10 17:36:56 +0900

    Update TopDownRotation.cs

--- Assets/Scripts/TopDownRotation.cs
+++ Assets/Scripts/TopDownRotation.cs
@@ -19,13 +19,23 @@ public class TopDownRotation : MonoBehaviour
 
     private void OnFlip(Vector2 direction)
     {
-        RotatePlayer(direction);
-    }
+        //RotatePlayer(direction);
 
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        characterRenderer.flipX = Mathf.Abs(rotZ) > 90f;
+    }
+    
+    // 플레이어의 방향을 바꾸는 기능 하나 뿐이므로 다시 메서드를 만들지 않고
+    // 바로 OnFlip에서 바꾸도록 하였음.
+
+    /*
     private void RotatePlayer(Vector2 direction)
     {
         float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
         characterRenderer.flipX = Mathf.Abs(rotZ) > 90f;
     }
+    */
+
 }

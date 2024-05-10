5bbe58ceca6380a6c1cda1d3623e459126cb6276
myu9977 <148519904+myu9977@users.noreply.github.com>
2024-05-10 19:44:07 +0900

    실행시 이름 입력

---
+++ Assets/Scripts/JoinController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class JoinController : MonoBehaviour
+{
+    public InputField nameInputField; // 사용자 이름을 입력하는 InputField
+    public Button joinButton; // Join 버튼
+
+    private void Start()
+    {
+        joinButton.onClick.AddListener(OnJoinButtonClick);
+    }
+
+    public void OnJoinButtonClick()
+    {
+        string playerName = nameInputField.text;
+
+        if (playerName.Length >= 2 && playerName.Length <= 10)
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+        else
+        {
+            Debug.Log("2~10 사이 글자수로 입력하세요.");
+        }
+    }
+}

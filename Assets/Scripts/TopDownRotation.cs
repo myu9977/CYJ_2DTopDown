8541402c95fa2a08df1b5ec1cfdf771a5ee51c38
myu9977 <148519904+myu9977@users.noreply.github.com>
2024-05-10 17:27:15 +0900

    플레이어는 마우스의 방향을 좌우 바라

---
+++ Assets/Scripts/TopDownRotation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+public class TopDownRotation : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    private TopDownController controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<TopDownController>();
+    }
+
+    private void Start()
+    {
+        controller.OnLookEvent += OnFlip;
+    }
+
+    private void OnFlip(Vector2 direction)
+    {
+        RotatePlayer(direction);
+    }
+
+    private void RotatePlayer(Vector2 direction)
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        characterRenderer.flipX = Mathf.Abs(rotZ) > 90f;
+    }
+}

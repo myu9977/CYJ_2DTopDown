82635cf60001c2ecaa911d72548d5857d9d66300
myu9977 <148519904+myu9977@users.noreply.github.com>
2024-05-10 15:13:22 +0900

    플레이어 이동

---
+++ Assets/Scripts/TopDownMovement.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+public class TopDownMovement : MonoBehaviour
+{
+    // 실제로 이동이 일어날 컴포넌트
+    
+    private TopDownController controller;
+    private Rigidbody2D movementRigidbody;
+
+    private Vector2 movementDirection = Vector2.zero;
+
+    private void Awake()
+    {
+        controller = GetComponent<TopDownController>();
+        movementRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        controller.OnMoveEvent += Move;
+    }
+
+    private void Move(Vector2 direction)
+    {
+        movementDirection = direction;
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement(movementDirection);
+    }
+
+    private void ApplyMovement(Vector2 direction)
+    {
+        direction = direction * 5;
+        movementRigidbody.velocity = direction;
+    }
+}
